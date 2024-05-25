d1b0a7038b5c8078354edaa7c615699de381b680
ksayounes <ksassouayounes2020@gmail.com>
2024-05-25 17:17:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
+  function navigateToSecondPage() {
+    window.location.href = 'second-page.html';
+}
+function showBeautifulMessage() {
+  alert("You are beautiful!");
+}
